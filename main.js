f6952ba3e0200c10db9142d09cc2527157b06c95
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2023-09-10 22:27:05 +0300

    feat: first version of calculator
    There is some bug with Del button. Need to change method of determining
    position of math operator.

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 const buttons = document.querySelectorAll("button");
 let displayMain = document.querySelector("#display :last-child");
 let displaySub = document.querySelector("#display :first-child");
-let switcher = false;
-buttons.forEach(e => {
+let operatorIsLast = false;
+buttons.forEach((e) => {
   e.addEventListener("click", () => {
-    if(e.getAttribute("class") === "number"){
-      !switcher ? displayMain.innerText += e.innerText : displayMain.innerText += ` ${e.innerText}`;
-      switcher = false;
+    if (e.getAttribute("class") === "number") {
+      !operatorIsLast
+        ? (displayMain.innerText += e.innerText)
+        : (displayMain.innerText += ` ${e.innerText}`);
+      operatorIsLast = false;
     }
-    if(e.getAttribute("class") === "math-operators"){
-      if (switcher || displayMain.innerText === "") return;
-      displayMain.innerText +=` ${e.innerText} `;
-      switcher = true;
+    if (e.getAttribute("class") === "math-operators") {
+      if (operatorIsLast || displayMain.innerText === "" || displayMain.innerText.split(" ").length === 3) return;
+      displayMain.innerText += ` ${e.innerText}`;
+      operatorIsLast = true;
     }
-  })
+    if (e.getAttribute("id") === "equal-button") {
+      const arr = displayMain.innerText.split(" ");
+      displaySub.innerText = displayMain.innerText;
+      displayMain.innerText = mathOperators[arr[1]](+arr[0], +arr[2]);
+    }
+    if (e.getAttribute("id") === "all-clear") {
+      displayMain.innerText = "";
+      displaySub.innerText = "";
+      operatorIsLast = false;
+    }
+    if (e.getAttribute("id") === "clear"){
+      let arr = displayMain.innerText.split("");
+      arr.pop();
+      displayMain.innerText = arr.join("");
+    }
+  });
 });
 
 let mathOperators = {
   "+": (a, b) => a + b,
   "-": (a, b) => a - b,
   "*": (a, b) => a * b,
-  "/": (a, b) => a / b
-}
+  "/": (a, b) => a / b,
+};
 
 // function mathChecker(){
 //   const arr = displayMain.innerText.split(' ');
