080c55f2b33e47e81fd3684c334599bad31aa745
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2023-09-11 19:48:57 +0300

    feat: Enable floating numbers

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 const buttons = document.querySelectorAll("button");
-let displayMain = document.querySelector("#display :last-child");
-let displaySub = document.querySelector("#display :first-child");
+const displayMain = document.querySelector("#display :last-child");
+const displaySub = document.querySelector("#display :first-child");
 buttons.forEach((e) => {
   e.addEventListener("click", () => {
-    if (e.getAttribute("class") === "number" || e.getAttribute("id") === "dote-button") {
+    if (e.getAttribute("class") === "number") {
       displayMain.innerText.split(" ").length < 2
         ? (displayMain.innerText += e.innerText)
-        : displayMain.innerText.split(" ").length === 2 
+        : displayMain.innerText.split(" ").length === 2
         ? (displayMain.innerText += ` ${e.innerText}`)
         : (displayMain.innerText += e.innerText);
     }
+    if (e.getAttribute("id") === "dote-button") {
+      const arr = displayMain.innerText.split(" ");
+      if (
+        (arr.length < 2 && arr[0].split("").some((item) => item === ".")) ||
+        (arr.length > 2 && arr[2].split("").some((item) => item === ".")) ||
+        arr.length === 2
+      ) {
+        return;
+      } else displayMain.innerText += e.innerText;
+    }
     if (e.getAttribute("class") === "math-operators") {
-      if (displayMain.innerText === "" || displayMain.innerText.split(" ").length >= 2) return;
+      if (
+        displayMain.innerText === "" ||
+        displayMain.innerText.split(" ").length >= 2
+      )
+        return;
       displayMain.innerText += ` ${e.innerText}`;
     }
     if (e.getAttribute("id") === "equal-button") {
@@ -19,8 +33,11 @@ buttons.forEach((e) => {
       const arr = displayMain.innerText.split(" ");
       for (i = 0; i < operators.length; i++) {
         while (arr.includes(operators[i])) {
-          let operator = arr.findIndex(item => item === operators[i]);
-          let result = mathOperators[arr[operator]](+arr[operator-1], +arr[operator+1]);
+          let operator = arr.findIndex((item) => item === operators[i]);
+          let result = mathOperators[arr[operator]](
+            +arr[operator - 1],
+            +arr[operator + 1]
+          );
           arr.splice(operator - 1, 3, result);
         }
       }
@@ -31,7 +48,7 @@ buttons.forEach((e) => {
       displayMain.innerText = "";
       displaySub.innerText = "";
     }
-    if (e.getAttribute("id") === "clear"){
+    if (e.getAttribute("id") === "clear") {
       let arr = displayMain.innerText.split("");
       arr.pop();
       displayMain.innerText = arr.join("");
