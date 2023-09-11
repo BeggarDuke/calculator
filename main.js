f8b6ab138593b2cc69a5de958751b0f40ec86e21
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2023-09-11 14:01:28 +0300

    feat: rework calculation method
    With new method i can build extended calculator that can evaluate
    more than one expression at a time.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let displayMain = document.querySelector("#display :last-child");
 let displaySub = document.querySelector("#display :first-child");
 buttons.forEach((e) => {
   e.addEventListener("click", () => {
-    if (e.getAttribute("class") === "number") {
+    if (e.getAttribute("class") === "number" || e.getAttribute("id") === "dote-button") {
       displayMain.innerText.split(" ").length < 2
         ? (displayMain.innerText += e.innerText)
         : displayMain.innerText.split(" ").length === 2 
@@ -15,9 +15,17 @@ buttons.forEach((e) => {
       displayMain.innerText += ` ${e.innerText}`;
     }
     if (e.getAttribute("id") === "equal-button") {
+      const operators = ["/", "*", "+", "-"];
       const arr = displayMain.innerText.split(" ");
+      for (i = 0; i < operators.length; i++) {
+        while (arr.includes(operators[i])) {
+          let operator = arr.findIndex(item => item === operators[i]);
+          let result = mathOperators[arr[operator]](+arr[operator-1], +arr[operator+1]);
+          arr.splice(operator - 1, 3, result);
+        }
+      }
       displaySub.innerText = displayMain.innerText;
-      displayMain.innerText = mathOperators[arr[1]](+arr[0], +arr[2]);
+      displayMain.innerText = arr;
     }
     if (e.getAttribute("id") === "all-clear") {
       displayMain.innerText = "";
@@ -37,8 +45,3 @@ let mathOperators = {
   "*": (a, b) => a * b,
   "/": (a, b) => a / b,
 };
-
-// function mathChecker(){
-//   const arr = displayMain.innerText.split(' ');
-//   return arr.length === 3 ? 3 : arr.length === 2 ? 2 : 0;
-// }
